334515d002e295f13fc0363710224942f0ff8671
Sergey Gorokhov <sergey.login@gmail.com>
2019-08-05 22:21:42 +0300

    Create database on app init

--- app.rb
+++ app.rb
@@ -3,6 +3,21 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'pony'
 require 'yaml'
+require 'sqlite3'
+
+configure do
+@db = SQLite3::Database.new 'barbershop.db'
+@db.execute 'CREATE TABLE IF NOT EXISTS "Users" (
+  "Id"  INTEGER PRIMARY KEY AUTOINCREMENT,
+  "Name"  TEXT,
+  "Phone" TEXT,
+  "DateStamp" TEXT,
+  "Barber"  TEXT,
+  "Color" TEXT
+)'
+
+end
+
 
 configure do
   enable :sessions
